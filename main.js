edeee06fd17ec4204c52d296e027c65073b57303
Jamie Randall <bentleyx007@gmail.com>
2019-07-15 14:59:15 -0700

    Complete

---
+++ main.js
@@ -0,0 +1,37 @@
+$(document).ready(() => {
+
+    let color = Math.floor(100000 + Math.random() * 900000)
+
+    setInterval(function() {
+        var today = new Date();
+        var hr = today.getHours();
+        var min = today.getMinutes();
+        var sec = today.getSeconds();
+
+        if (hr > 12) {
+            hr = hr - 12;
+        }
+
+        if (hr < 10) {
+            hr = "0" + hr;
+        }
+
+        if (min < 10) {
+            min = "0" + min;
+        }
+        
+        if (sec < 10) {
+            sec = "0" + sec;
+        }
+
+        var time = hr + ":" + min + ":" + sec;
+
+        $("#clock").html(time)
+
+        let newColor = color += 1 
+        finalColor = '#' + newColor
+        $("#hex").html(finalColor)
+
+        document.body.style.backgroundColor = finalColor; 
+    },1000)
+})
